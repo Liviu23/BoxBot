eb09219ad1135c13bea395d87c82e86be63056d4
Liviu Stanescu <liviu23cristian@gmail.com>
2020-05-03 13:24:17 +0300

    Added code to see if bot works

---
+++ ConsoleApp1/Commands.cs
@@ -0,0 +1,26 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("echo", RunMode = RunMode.Async)]
+        [Alias("say")]
+        public async Task Echo([Remainder]string msg)
+        {
+            await ReplyAsync(msg);
+        }
+
+        [Command("random", RunMode = RunMode.Async), Alias("rand")]
+
+        public async Task GetRandomNumber(int maxVal, int minVal = 0)
+        {
+            await ReplyAsync(new Random().Next(minVal, maxVal).ToString());
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,19 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Reflection;
+using Discord.WebSocket;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var box = new Box(Box.GetEssentials().BuildServiceProvider());
+            await AddCommandsAsync(box);
             box.Config.ClientType = ClientType.Socket;
             box.Config.DiscordToken = GetToken("token.txt");
-            
+            box.SetDiscordSocketConfig(new DiscordSocketConfig()
+            {
+                WebSocketProvider = Discord.Net.Providers.WS4Net.WS4NetProvider.Instance,
+                LogLevel = Discord.LogSeverity.Info
+            });
+
+            box.StartAsync();
+            while(true)
+            {
+                switch(Console.ReadLine())
+                {
+                    case "exit":
+                        box.Stop();
+                        return;
+                }
+            }
         }
 
+        static async Task AddCommandsAsync(Box box) => await box.AddCommandModulesAsync(Assembly.GetExecutingAssembly());
 
         static string GetToken(string path) => File.ReadAllText(path);
     }
